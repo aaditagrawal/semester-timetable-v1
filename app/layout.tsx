152339a4896e2f27c352543b40930c98bd28838e
sherlock <aadit@aaditagrawal.com>
2026-01-02 22:26:48 +0530

    feat: added analytics endpoint

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, JetBrains_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const jetbrainsMono = JetBrains_Mono({ subsets: ['latin'], variable: '--font-sans' });
@@ -29,6 +30,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Script defer src="https://stat.sys256.com/script.js" strategy="afterInteractive" />
         {children}
       </body>
     </html>
